c3e3d2581969d0e67faa2f96cbb72e8ce52b4e66
Piotrek <piotr.kowalewski@gmail.com>
2019-08-14 15:16:28 +0200

    Init commit

---
+++ src/main/java/pl/rawinet/detal/DetalApplication.java
@@ -0,0 +1,13 @@
+package pl.rawinet.detal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetalApplication.class, args);
+	}
+
+}
